3a9419787911110a73eb06059eab07f0d1c4593a
stang <chokilover3@gmail.com>
2022-09-10 15:36:49 +0700

    1.0.6

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ var product = [{
 $(document).ready(() => {
     var html = '';
     for (let i = 0; i < product.length; i++) {
-        html += `<div onclick="openProductDetail(${i})" class="${product[i].type} bg-white p-2 cursor-pointer transition delay-100 hover:scale-105 hover:shadow-xl product-items">
+        html += `<div onclick="openProductDetail(${i})" class="${product[i].type} bg-white p-2 cursor-pointer transition delay-100 hover:scale-105 hover:shadow-xl product-items hover:rounded-xl">
                     <img src="${product[i].img}" class="object-cover rounded-xl">
                     <p>${product[i].name}</p>
                     <p>${ numberWithCommas(product[i].price)} THB</p>
@@ -68,4 +68,109 @@ function openProductDetail(index) {
 
 function closeModal() {
     $(".modal").css('display','none')
+}
+
+var cart = []
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if( productindex == cart[i].index) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        cart.push(obj)
+    }
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="flex justify-between mb-2.5">
+                        <div class="flex">
+                            <img src="${cart[i].img}" alt="" class="object-cover w-24 rounded-md">
+                            <div class="ml-2.5">
+                                <p">${cart[i].name}</p>
+                                <p>${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <p onclick="deinitems('-', ${i})" class="border border-black px-2 rounded-3xl cursor-pointer">-</p>
+                            <p id="countitems${i}" class="mx-2">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class=" border border-black px-2 rounded-3xl cursor-pointer">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
+                })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
 }
